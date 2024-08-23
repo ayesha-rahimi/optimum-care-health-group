047a0e5b65db58d9eaed0c0fd701004b0cf8da49
Nitin Surya <nitinsuryaimadabathuni@gmail.com>
2024-08-23 14:43:09 -0500

    charts data change

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -19,34 +19,84 @@ const Dashboard = () => {
       stateAvg: 3.76,
       doctorAvg: 3.5,
       type: "bar",
-      link: "https://www.definitivehc.com/resources/healthcare-insights/hcahps-scores-state#:~:text=What%20is%20the%20average%20HCAHPS,tracked%20in%20HospitalView%20is%203.33.",
-      linkDesc: "HCAHPS scores",
+      reference: [
+        {
+          link: "https://www.definitivehc.com/resources/healthcare-insights/hcahps-scores-state#:~:text=What%20is%20the%20average%20HCAHPS,tracked%20in%20HospitalView%20is%203.33.",
+          linkDesc: "HCAHPS scores",
+        },
+      ],
     },
     {
       name: "Quality",
-      description:
-        "Overall quality rating of the healthcare services provided.",
+      description: `Quality measures indicate a reduction in hospital-acquired infections (HAIs), with 
+a 14% decrease in central line-associated bloodstream infections (CLABSIs) in 
+2023. 
+The rate of surgical site infections (SSIs) for select procedures dropped by 9% 
+nationally.
+Oklahoma hospitals also showed improvements in HAIs, with a 12% reduction in 
+CLABSIs. The state has been actively participating in CMS's Hospital Improvement 
+Innovation Networks (HIINs), which contributed to these gains. Source: .
+`,
       nationalAvg: 78,
       stateAvg: 82,
       doctorAvg: 88,
-      type: "pie",
+      type: "bar",
+      reference: [
+        {
+          link: "https://www.cms.gov/medicare/quality/initiatives/hospital-quality-initiative/hospital-compare",
+          linkDesc:
+            "CMS Hospital Quality Initiative (Centers for Medicare & Medicaid Services)",
+        },
+
+        {
+          link: "https://www.cdc.gov/nchs/nhcs",
+          linkDesc: "National Hospital Care Survey (CDC)",
+        },
+      ],
     },
     {
       name: "Return to Acute",
-      description:
-        "Rate at which patients return to acute care within 30 days.",
-      nationalAvg: 10,
-      stateAvg: 9,
+      description: `The 30-day all-cause hospital readmission rate remained largely stable in 2020 
+compared to previous years, with a slight reduction of 10.2% in the total number 
+of readmissions across the U.S. 
+
+Similar to national trends, Oklahoma saw a reduction in readmissions, particularly in 
+rural areas where healthcare initiatives targeted reducing acute care returns. Specific 
+data shows that rural hospitals in Oklahoma reduced their readmission rates by 
+approximately 9.3%.`,
+      nationalAvg: 13.9,
+      stateAvg: 9.3,
       doctorAvg: 7,
-      type: "doughnut",
+      type: "bar",
+      reference: [
+        {
+          link: "https://hcup-us.ahrq.gov/reports/statbriefs/sb304-readmissions-2016-2020.jsp",
+          linkDesc: "Healthcare Cost and Utilization Project (HCUP)",
+        },
+      ],
     },
     {
       name: "Length of Stay",
-      description: "Average length of stay for patients in the hospital.",
-      nationalAvg: 5.5,
-      stateAvg: 5.0,
+      description: `The average length of stay in U.S. hospitals in 2023 was 4.6 days, a slight decrease 
+from 4.7 days in 2022. 
+In Oklahoma, the average length of stay mirrored national trends at 4.7 days, with 
+rural hospitals reporting slightly longer stays due to limited post-acute care 
+options.
+`,
+      nationalAvg: 4.6,
+      stateAvg: 4.7,
       doctorAvg: 4.5,
       type: "bar",
+      reference: [
+        {
+          link: "https://www.aha.org/statistics/fast-facts-us-hospitals",
+          linkDesc: " American Hospital Association",
+        },
+        {
+          link: "https://www.cms.gov/newsroom/fact-sheets/acute-hospital-care-home-data-release-fact-sheet",
+          linkDesc: " CMS Acute Hospital Care at Home Data ",
+        },
+      ],
     },
   ];
 
@@ -85,11 +135,22 @@ const Dashboard = () => {
           <div>
             <h2>{selectedMetric.name}</h2>
             <p>{selectedMetric.description}</p>
-            {selectedMetric.link && (
+
+            {selectedMetric.reference &&
+              selectedMetric.reference.map((item, index) => (
+                <>
+                  <a href={item.link} target="_blank" rel="noreferrer">
+                    {item.linkDesc}
+                  </a>
+                  <br /> <br />
+                </>
+              ))}
+
+            {/* {selectedMetric.link && (
               <a href={selectedMetric.link} target="_blank" rel="noreferrer">
                 {selectedMetric.linkDesc}
               </a>
-            )}
+            )} */}
           </div>
         )}
       </Dialog>
